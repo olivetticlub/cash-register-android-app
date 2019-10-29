b287b92a4734ca68ebde66e02f6878d718fd8850
XPeppers <nicola.margoni@xpeppers.com>
2019-10-29 14:10:15 +0100

    import bluetooth printer ''library''

--- app/src/main/java/com/olivetticlub/cashregisterapp/transaction/TransactionFragment.kt
+++ app/src/main/java/com/olivetticlub/cashregisterapp/transaction/TransactionFragment.kt
@@ -6,8 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.olivetticlub.cashregisterapp.R
+import com.olivetticlub.cashregisterapp.printer.thermal.Printer
+import com.olivetticlub.cashregisterapp.printer.thermal.bluetooth.BluetoothPrinters
+import kotlinx.android.synthetic.main.fragment_transaction.*
 
 class TransactionFragment : Fragment() {
+
+    private lateinit var printer: Printer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        printer = Printer(BluetoothPrinters().list.first(), 203, 58.0f, 42)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +26,16 @@ class TransactionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_transaction, container, false)
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+        printReceiptButton.setOnClickListener {
+            printer.printFormattedText("[L]1 x Cazzettino\n")
+        }
     }
 
 }
